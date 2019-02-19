78f9489074cf95a635738d1bb972e3139446695a
micheal cannon <cannonmicheal@hotmail.com>
2019-02-19 11:58:54 +0000

    first commit

---
+++ sumupto.py
@@ -0,0 +1,10 @@
+#  Problem Set Question 1
+# A program that asks the user to input a postive integer and outputs the sum of all numbers between 1 and that number
+
+i = int(input("Please enter a positive integer: "))
+n = i - 1
+while n > 0:
+    i = i + n
+    n = n - 1
+
+print(i)

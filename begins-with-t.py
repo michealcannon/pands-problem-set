6537d81dcbeaa1f7a12cef8cfde470fe545deb2a
micheal cannon <cannonmicheal@hotmail.com>
2019-02-22 22:41:39 +0000

    first commit

---
+++ begins-with-t.py
@@ -0,0 +1,17 @@
+# Micheál Cannon - problem set question 1
+# A program that outputs whether or not today is a day that begins with the letter T
+
+ # import the datetime module
+
+import datetime
+# check if today is Tuesday and print confirmation that today begins with a t
+if datetime.datetime.today().weekday() == 1:
+     print("Yes - today begins with a T")
+
+# check if today is Thursday and print confirmation that today begins with a t
+elif datetime.datetime.today().weekday() == 3:
+     print("Yes - today begins with a T")
+
+# if today isn't Tuesday or Thursday, print that today doesn't begin with a t
+else:
+    print("No - today does not begin with a T")
